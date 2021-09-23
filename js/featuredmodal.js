ae48da9790a14c0a3b029e7cb42327e58a6c73ea
sebbe1236 <74545889+sebbe1236@users.noreply.github.com>
2021-09-23 13:41:19 +0200

    cleaup of code

--- js/featuredmodal.js
+++ js/featuredmodal.js
@@ -1,4 +1,4 @@
-let imageOverlay = document.querySelector(".overlay");
+//let imageOverlay = document.querySelector(".overlay");
 
 let onclickImage;
 let isOpen = false;
@@ -10,20 +10,15 @@ function init() {
 
 function openModal() {
   if (isOpen === false) {
-    //onclickImage.style.transform = "scale(1.3)";
     onclickImage.classList.add("modal-open");
-    //onclickImage.classList.add("overlay"); modal overlay box som er display hidden og kommentert ut i CSSn nå.
-    //onclickImage.classList.add("overlay");
-    //onclickImage.className += "imageModal"; class som er lagt til som skulle align`e bilde boxn.
-    //imageOverlay.style.display = "block";
+    //onclickImage.classList.add("overlay"); //modal overlay box som er display hidden og kommentert ut i CSSn nå.
+
+    //onclickImage.className += "imageModal"; //class som er lagt til som skulle align`e bilde boxn.
+    //onclickImage.style.display = "block";
     //onclickImage.style.width = "80%";
     //onclickImage.style.height = "80%";
-
-    //onclickImage.style.transition = "transform 0.50s ease";
   } else {
-    //onclickImage.style.transform = "scale(1.0)";
-    //onclickImage.style.transition = "transform 0.50s ease";
-    //onclickImage.classList.remove("overlay"); modal overlay box
+    //onclickImage.classList.remove("overlay"); //modal overlay box
     onclickImage.classList.remove("modal-open");
   }
 
--- js/script.js
+++ js/script.js
@@ -1,14 +1,11 @@
-//https://exam1api.sebbeprojects.com/wp-json/wp/v2/posts;
-
 const query = document.querySelector(".query-content");
 
 const queryString = document.location.search;
 const param = new URLSearchParams(queryString);
 const id = param.get("id");
-//const urls = "https://exam1api.sebbeprojects.com/wp-json/wp/v2/posts?_embed=true" + id;
+
 const urls = "https://exam1api.sebbeprojects.com/wp-json/wp/v2/posts/" + id;
-//const urls = "https://exam1api.sebbeprojects.com/wp-json/wp/v2/posts?_embed" + id correct url but you have to edit the php to get iit tto woork
-//https://developer.wordpress.org/rest-api/frequently-asked-questions/;
+
 console.log(id);
 
 async function idFetch() {
@@ -16,13 +13,10 @@ async function idFetch() {
     const response = await fetch(urls);
     const details = await response.json();
     return details;
-    console.log(details);
-    //productFetch(details);
   } catch (error) {
     console.log("jajajja");
   }
 }
-//idFetch();
 
 function productFetch(details) {
   //console.log(details);
@@ -38,6 +32,3 @@ function productFetch(details) {
 idFetch().then((content) => {
   productFetch(content);
 });
-//<img src="${details._embedded["wp:featuredmedia"]["0"].source_url}" alt= "test"> dette er bildelenken som skal limes inn i product fetch div`n
-//img src="${details.guid.rendered}" funker ikke men henter et img.
-//media_details.sizes.thumbnail.source_url
